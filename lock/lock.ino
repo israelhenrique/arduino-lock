a4e03d20bd3b6cf9df50d65059ebeb5ff79c5e33
Israel Lima <israelhenrique@gmail.com>
2016-09-06 15:11:12 -0300

    add function isOpen

--- lock/lock.ino
+++ lock/lock.ino
@@ -8,7 +8,15 @@ int ldrValor = 0;
 //Inicializa a biblioteca utilizando as portas de 8 a 11 para 
 //ligacao ao motor 
 Stepper myStepper(stepsPerRevolution, 8,10,9,11); 
-  
+ 
+int isOpen(int value){
+
+  if (value > 820)
+    return 0;
+  else
+    return 1;
+}
+
 void setup() 
 { 
  //Determina a velocidade inicial do motor 
@@ -21,8 +29,10 @@ void loop()
 { 
  ldrValor = analogRead(ldrPin);
  Serial.println(ldrValor);
+ 
  if ( ldrValor < 820)
    myStepper.step(682); 
+ 
  delay(10); 
  
 }
