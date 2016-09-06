b9aa158ec8db5b29f589b17a44c9a2fc7e76c7b3
Israel Lima <israelhenrique@gmail.com>
2016-09-06 15:07:04 -0300

    ldr stop stepper

--- lock/lock.ino
+++ lock/lock.ino
@@ -1,6 +1,9 @@
 #include <Stepper.h> 
  
 const int stepsPerRevolution = 500; 
+const int ldrPin = 5;
+const int ledPin = 13;
+int ldrValor = 0;
   
 //Inicializa a biblioteca utilizando as portas de 8 a 11 para 
 //ligacao ao motor 
@@ -10,12 +13,16 @@ void setup()
 { 
  //Determina a velocidade inicial do motor 
  myStepper.setSpeed(60);
+ pinMode(ledPin,OUTPUT);
+ Serial.begin(9600);
 } 
   
 void loop() 
 { 
-
- myStepper.step(682); 
- delay(100); 
+ ldrValor = analogRead(ldrPin);
+ Serial.println(ldrValor);
+ if ( ldrValor < 820)
+   myStepper.step(682); 
+ delay(10); 
  
 }
