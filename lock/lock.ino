b9eb5fa1510fc3f96752b6196e9d765bcca3df9d
Israel Lima <israelhenrique@gmail.com>
2016-09-06 15:48:41 -0300

    add 7seg display

--- lock/lock.ino
+++ lock/lock.ino
@@ -3,6 +3,9 @@
 const int stepsPerRevolution = 500; 
 const int ldrPin = 5;
 const int buttonPin = 2;
+const int clockPin = 3;
+const int latchPin = 4;
+const int dataPin = 5;
 int ldrValor = 0;
 int stepperOn = 0;
   
@@ -18,16 +21,30 @@ int isOpen(int value){
     return 1;
 }
 
+void setupDisplay(int value){
+
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, value);
+  digitalWrite(latchPin, HIGH);
+
+}
+
 void setup() 
 { 
  //Determina a velocidade inicial do motor 
  myStepper.setSpeed(60);
  pinMode(buttonPin,INPUT);
+ pinMode(latchPin, OUTPUT);
+ pinMode(clockPin, OUTPUT);
+ pinMode(dataPin, OUTPUT);  
  Serial.begin(9600);
 } 
   
 void loop() 
 { 
+  
+  
+  
  ldrValor = analogRead(ldrPin);
  Serial.println(ldrValor);
  int buttonValue = digitalRead(buttonPin);
