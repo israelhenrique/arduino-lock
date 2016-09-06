a2a997a063e0eacc66d9ba901954047bff6e8291
Israel Lima <israelhenrique@gmail.com>
2016-09-06 15:24:33 -0300

    button starts stepper

--- lock/lock.ino
+++ lock/lock.ino
@@ -2,8 +2,9 @@
  
 const int stepsPerRevolution = 500; 
 const int ldrPin = 5;
-const int ledPin = 13;
+const int buttonPin = 2;
 int ldrValor = 0;
+int stepperOn = 0;
   
 //Inicializa a biblioteca utilizando as portas de 8 a 11 para 
 //ligacao ao motor 
@@ -21,7 +22,7 @@ void setup()
 { 
  //Determina a velocidade inicial do motor 
  myStepper.setSpeed(60);
- pinMode(ledPin,OUTPUT);
+ pinMode(buttonPin,INPUT);
  Serial.begin(9600);
 } 
   
@@ -29,8 +30,14 @@ void loop()
 { 
  ldrValor = analogRead(ldrPin);
  Serial.println(ldrValor);
+ int buttonValue = digitalRead(buttonPin);
  
- if ( ldrValor < 820)
+ if(isOpen(ldrValor) && buttonValue == HIGH)
+   stepperOn = 1;
+ else if(!isOpen(ldrValor))
+   stepperOn = 0;
+ 
+ if (stepperOn)
    myStepper.step(682); 
  
  delay(10); 
